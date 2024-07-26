e5c7f2aa26ef7aec9d7690ad56e24725b79d6da7
jmx3133 <jameshart3133@gmail.com>
2024-07-26 12:51:20 +1000

    update g2

--- controllers/cronjob_controller.go
+++ controllers/cronjob_controller.go
@@ -1,56 +1,106 @@
-/*
-Copyright 2024.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controllers
 
 import (
 	"context"
 
+	batchv1alpha1 "github.com/JohnWong9911/my-operator/api/v1"
+	"github.com/go-logr/logr"
+	batchv1core "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/utils/clock"
-
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	batchv1 "github.com/JohnWong9911/my-operator/api/v1"
 )
 
 // CronJobReconciler reconciles a CronJob object
 type CronJobReconciler struct {
 	client.Client
+	Log    logr.Logger
 	Scheme *runtime.Scheme
-	Clock  clock.Clock
 }
 
-// +kubebuilder:rbac:groups=batch.my.domain,resources=cronjobs,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=batch.my.domain,resources=cronjobs/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=batch.my.domain,resources=cronjobs/finalizers,verbs=update
+//+kubebuilder:rbac:groups=batch.example.com,resources=cronjobs,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=batch.example.com,resources=cronjobs/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=batch.example.com,resources=cronjobs/finalizers,verbs=update
+//+kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
 
 func (r *CronJobReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := r.Log.WithValues("cronjob", req.NamespacedName)
 
-	// your logic here
+	// Fetch the CronJob instance
+	cronJob := &batchv1alpha1.CronJob{}
+	err := r.Get(ctx, req.NamespacedName, cronJob)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Object not found, return. Created objects are automatically garbage collected.
+			// For additional cleanup logic use finalizers.
+			log.Info("CronJob resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get CronJob")
+		return ctrl.Result{}, err
+	}
 
+	// Define the job from the cronjob's spec
+	job := &batchv1core.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cronJob.Name + "-job",
+			Namespace: cronJob.Namespace,
+		},
+		Spec: batchv1core.JobSpec{
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"cronjob-name": cronJob.Name,
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:    "cronjob-container",
+							Image:   "busybox",
+							Command: cronJob.Spec.Command,
+						},
+					},
+					RestartPolicy: corev1.RestartPolicyOnFailure,
+				},
+			},
+		},
+	}
+
+	// Set the cronjob instance as the owner and controller
+	if err := ctrl.SetControllerReference(cronJob, job, r.Scheme); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// Check if the Job already exists
+	found := &batchv1core.Job{}
+	err = r.Get(ctx, types.NamespacedName{Name: job.Name, Namespace: job.Namespace}, found)
+	if err != nil && errors.IsNotFound(err) {
+		log.Info("Creating a new Job", "Job.Namespace", job.Namespace, "Job.Name", job.Name)
+		err = r.Create(ctx, job)
+		if err != nil {
+			log.Error(err, "Failed to create new Job", "Job.Namespace", job.Namespace, "Job.Name", job.Name)
+			return ctrl.Result{}, err
+		}
+		// Job created successfully - return and requeue
+		return ctrl.Result{Requeue: true}, nil
+	} else if err != nil {
+		log.Error(err, "Failed to get Job")
+		return ctrl.Result{}, err
+	}
+
+	// Job already exists - don't requeue
+	log.Info("Skip reconcile: Job already exists", "Job.Namespace", found.Namespace, "Job.Name", found.Name)
 	return ctrl.Result{}, nil
 }
 
-// SetupWithManager sets up the controller with the Manager.
 func (r *CronJobReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&batchv1.CronJob{}).
+		For(&batchv1alpha1.CronJob{}).
 		Complete(r)
 }
