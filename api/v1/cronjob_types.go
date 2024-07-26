1b386332829b70b6d67644bef0e3011533bb90a8
jmx3133 <jameshart3133@gmail.com>
2024-07-26 13:02:24 +1000

    update g2

--- api/v1/cronjob_types.go
+++ api/v1/cronjob_types.go
@@ -6,6 +6,11 @@ import (
 
 // CronJobSpec defines the desired state of CronJob
 type CronJobSpec struct {
+	// ContainerName is the name of the container
+	ContainerName string `json:"containerName"`
+
+	// Image is the container image to run
+	Image string `json:"image"`
 	// Schedule is the cron schedule
 	Schedule string `json:"schedule"`
 
